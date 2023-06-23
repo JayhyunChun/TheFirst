3d7ee08be42ef5b24cbe642ea6f1a96660287ae3
JayhyunChun <96852531+JayhyunChun@users.noreply.github.com>
2023-06-24 02:33:50 +0900

    Update pa1.cpp

--- pa1.cpp
+++ pa1.cpp
@@ -2,42 +2,225 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <algorithm>
+#include <vector>
+#include <tuple>
 
 using namespace std;
 
+int bandwCap;                               // Bandwidth cap
+
 int main(int argc, char* argv[]) {
-    ifstream inputFile("test.txt");  // Open the file for reading
+    int numberOfLines;                      // The number of lines
+    vector<tuple<int, int, int>> clients;
+    /*
+    The list of clients:
+    1st int: The index of clients
+    2nd int: Expected Revenue
+    3rd int: Expected BW
+    */   
+
+    int maxBW = 0;                          // Maximum bandwidth
+    int maxRev = 0;                         // Maximum revenue
+
+    ifstream inputFile(argv[1]);            // Open the file for reading
 
     if (inputFile.is_open()) {
+        int counter = 0;                    // Determines the value inside the .txt file
+        int index = 0;                      // Determines the index of each client
         string line;
         while (getline(inputFile, line)) {  // Read lines from the file
-            istringstream iss(line);  // Create a string stream from the line
-
+            istringstream iss(line);
+            // Create a string stream from the line
+            
             string token;
+            
             while (getline(iss, token, ' ')) {
-                istringstream numStream(token); // Create a new stream to handle comma-separated values
+                istringstream numStream(token);
+                // Create a new stream to handle comma-separated values
 
                 string numToken;
+                
                 while (getline(numStream, numToken, ',')) {
                     // Remove any non-digit characters from the token
                     numToken.erase(remove_if(numToken.begin(), numToken.end(), [](char c) {
                         return !isdigit(c);
-                        }), numToken.end());
+                    }), numToken.end());
 
                     if (!numToken.empty()) {
-                        // Convert the token to an integer and print it
+                        // Convert the token to an integer and retrieve it
                         int number = stoi(numToken);
-                        cout << number << endl;
+                        
+                        /*
+                        When the counter value is:
+                        0:              bandwCap
+                        1:              numberOfLines
+                        more than 2:    clients
+                        */
+                        switch(counter)
+                        {
+                            case 0:
+                                bandwCap = number;
+                                break;
+                            case 1:
+                                numberOfLines = number;
+                                break;
+                            default:
+                                if (counter % 2 == 0) {
+                                    clients.push_back(make_tuple(index, number, 0));
+                                }
+                                else {
+                                    get<2>(clients[index]) = number;
+                                    maxBW += number;
+                                    index++;
+                                }
+
+                                break;
+                        }
                     }
+
+                    counter++;
                 }
             }
         }
 
-        inputFile.close();  // Close the file
+        inputFile.close();      // Close the file
     }
     else {
         cout << "Failed to open the file." << endl;
     }
+    // Done retrieving values from text file.
+
+    cout << "The bandwidth cap: " << bandwCap << endl << "Number of lines: "
+    << numberOfLines << endl;
+
+    for (const auto& element : clients) {
+        int index1 = get<0>(element);
+        int index2 = get<1>(element);
+        int index3 = get<2>(element);
+        cout << "(" << index1 << ", " << index2 << ", " << index3 << ")" << endl;
+    }
+
+    cout << "Maximum BW: " << maxBW << endl << endl;
+
+    int currentRev[bandwCap];                       // The revenue for calculating
+    int currentBW[bandwCap];                        // The BW for calculating
+    int theIndex = 0;
+    bool subleaseBool = false;                      // The bool of sublease
+
+    // Initializing the currentRev and currentBW array.
+    for (int i = 0; i < bandwCap; i++) {
+        currentRev[i] = 0;
+        currentBW[i] = 0;
+    }
+
+    int numberOfCalc = numberOfLines;               // Later determines how many calculations are needed.
+
+    for (int h = 0; h < numberOfLines - 1; h++) {
+        int indexes[numberOfCalc][numberOfCalc - 1];
+        /*
+        Determine "the" index.
+        The index indicates whether the calculation of sublease should be taken or not.
+        */
+
+        for (int i = 0; i < numberOfCalc; i++) {
+            int tmp = maxBW;
+            int index2 = 0;
+
+            maxBW -= get<2>(clients[i]);
+            currentBW[i] = maxBW;
+            cout << "maxBW - BW of client" << i << ": " << currentBW[i] << "\t";
+            
+            for (int j = 0; j < numberOfCalc; j++) {
+                if (i != j) {
+                    currentRev[i] += get<1>(clients[j]);
+                    indexes[i][index2] = get<0>(clients[j]);
+                    index2++;
+                }
+            }
+
+            cout << "currentRev: " << currentRev[i] << endl;
+
+            if (i == 0) {
+                maxRev = currentRev[i];
+                // Initially store the very first current revenue into the maximum revenue.
+            }
+            else {
+                if (currentRev[i] > maxRev) {
+                    maxRev = currentRev[i];
+                    theIndex = i;
+                }
+            }
+
+            maxBW = tmp;
+        }
+
+        cout << endl << "The maximum revenue: " << maxRev << endl;
+        cout << "theIndex: " << theIndex << endl;
+
+        for (int i = 0; i < numberOfCalc; i++) {
+            for (int j = 0; j < numberOfCalc - 1; j++) {
+                cout << indexes[i][j] << " ";
+            }
+
+            cout << endl;
+        }
+
+        if (currentBW[theIndex] <= bandwCap) {
+            ofstream dummyFile("dummy.txt");
+            
+            if (subleaseBool == false) {
+                dummyFile << "0" << endl << numberOfCalc - 1 << endl;
+            }
+            else {
+                cout << endl << "maxBW: " << maxBW << "\nbandwCap: " << bandwCap << endl;
+                dummyFile << "1" << endl << theIndex << ","
+                << maxBW - bandwCap << endl;
+            }
+
+            for (int i = 0; i < numberOfCalc - 1; i++) {
+                dummyFile << indexes[theIndex][i] << endl;
+            }
+
+            dummyFile.close();
+            break;
+        }
+        else {
+            numberOfCalc--;
+            subleaseBool = true;
+            maxBW -= get<2>(clients[theIndex]);
+            clients.erase(clients.begin() + theIndex);
+
+            for (int i = 0; i < bandwCap; i++) {
+                currentRev[i] = 0;
+                currentBW[i] = 0;
+            }
+
+            for (const auto& element : clients) {
+                int index1 = get<0>(element);
+                int index2 = get<1>(element);
+                int index3 = get<2>(element);
+                cout << "(" << index1 << ", " << index2 << ", " << index3 << ")" << endl;
+            }
+        }
+    }
+
+    // Encode the "dummy.txt" into an UTF-8 format "output.txt" file.
+    ifstream dummyFile("dummy.txt", ios::in | ios::binary);
+    ofstream outputFile("output.txt", ios::out | ios::binary);
+
+    string line;
+
+    while(getline(dummyFile, line)) {
+        outputFile << line << endl;
+    }
+
+    dummyFile.close();
+    outputFile.close();
 
     return 0;
+}
+
+int calcSublease(int rev, int remainedBW) {
+    return (double)(1.1 * (remainedBW / bandwCap) * rev);
 }
